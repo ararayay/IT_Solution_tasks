5359d4fb795421609f511ab48010836fe422be32
Raya <rayakuzmina73@gmail.com>
2025-07-17 17:44:04 +0300

    Add base.html, display 10 last deals and styles

--- manage_deals/views.py
+++ manage_deals/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpRequest, HttpResponse
 from integration_utils.bitrix24.bitrix_user_auth.main_auth import main_auth
+from integration_utils.bitrix24.functions.call_list_method import call_list_method
 from django.conf import settings
 
 
@@ -9,5 +10,11 @@ def home(request: HttpRequest) -> HttpResponse:
     app_settings = settings.APP_SETTINGS
     return render(request, 'home.html', locals())
 
+@main_auth(on_cookies=True)
 def last_deals(request: HttpRequest) -> HttpResponse:
-    return render(request, 'last_deals.html')
+    result = call_list_method(bx_token=request.bitrix_user_token,
+                                  method='crm.deal.list',
+                                  fields={'CLOSED': 'N',
+                                          'DATE_CREATE': 'DESC'})
+    last_deals = result[:10]
+    return render(request, 'last_deals.html', {'last_deals': last_deals})
