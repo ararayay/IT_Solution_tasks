6c68f8eabdf9093442c68885bd3f7103a3a11559
Raya <rayakuzmina73@gmail.com>
2025-07-18 14:38:09 +0300

    Add form for creating deal

--- manage_deals/urls.py
+++ manage_deals/urls.py
@@ -5,5 +5,6 @@ from . import views
 
 urlpatterns = [
     path("", views.home, name="home"),
-    path("/last-deals", views.last_deals, name="last_deals"),
+    path("last-deals", views.last_deals, name="last_deals"),
+    path("add-deal", views.add_deal, name="add_deal"),
 ]
